f2ef4b1f2f2b6e474e5303a27ba1ff58082a57cd
Riu-y <y1a1r2y5u@gmail.com>
2020-07-17 09:11:15 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class StaticPageControllerTest < ActionDispatch::IntegrationTest
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
 	def setup
 		@base_title = "Ruby on Rails Tutorial Sample App"
@@ -11,26 +11,26 @@ class StaticPageControllerTest < ActionDispatch::IntegrationTest
 	end
 
   test "should get home" do
-    get static_page_home_url
+    get home_path
     assert_response :success
     assert_select "title","#{@base_title}"
   end
 
   test "should get help" do
-    get static_page_help_url
+    get help_path
     assert_response :success
     assert_select "title","Help | #{@base_title}"
   end
 
   test "should get about" do
-  	get static_page_about_url
+  	get about_path
   	assert_response :success
   	assert_select "title","About | #{@base_title}"
   	assert_select "h1","About"
   end
 
   test "should get contact" do
-  	get static_page_contact_url
+  	get contact_path
   	assert_response :success
   	assert_select "title","Contact | #{@base_title}"
   end
