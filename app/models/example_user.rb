c2de6750b3f91dd2dce098092b42f1aecbb7055d
Riu-y <y1a1r2y5u@gmail.com>
2020-07-21 06:41:52 +0000

    「Modify]DB

--- app/models/example_user.rb
+++ app/models/example_user.rb
@@ -1,13 +1,13 @@
-class User
-	attr_accessor :name, :email
+# class User
+# 	attr_accessor :name, :email
 
-	def initialize(attributes = {})
-		@name = attributes[:name]
-		@email = attributes[:email]
-	end
+# 	def initialize(attributes = {})
+# 		@name = attributes[:name]
+# 		@email = attributes[:email]
+# 	end
 
 
-	def formatted_email
-		"#{@name}<#{@email}>"
-	end
-end
+# 	def formatted_email
+# 		"#{@name}<#{@email}>"
+# 	end
+# end
